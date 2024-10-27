f34a8f9f7ff7462dd6b0a5a62b3dbbdf437456d6
ahmadfarhanfatoni <fffarhan960@gmail.com>
2024-10-27 22:12:07 +0700

    transactions

--- src/routes/transaction.js
+++ src/routes/transaction.js
@@ -7,10 +7,6 @@ const {
   deleteTransaction,
 } = require("../controllers/transactions");
 const { verifyToken } = require("../middleware/verifyToken");
-const users = require("../models/Users");
-const products = require("../models/Products");
-const transactions = require("../models/Transactions");
-
 const transactionRouter = require("express").Router();
 
 transactionRouter.post(`/`,verifyToken, createTransaction);
@@ -21,7 +17,3 @@ transactionRouter.delete("/:id", deleteTransaction);
 
 module.exports = transactionRouter;
 
-transactions.belongsTo(users, {foreignkey : "userId", as : "user"})
-transactions.belongsTo(products, {foreignkey : "productId", as : "product"})
-users.hasMany(transactions, { foreignKey : "id"})
-products.hasMany(transactions, { foreignKey : "id"})
